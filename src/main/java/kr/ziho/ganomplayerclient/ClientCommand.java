380732adbc8ee7d9281af9b032b568870b1d1f77
piz2a <43025513+piz2a@users.noreply.github.com>
2024-05-02 14:15:22 +0900

    init

---
+++ src/main/java/kr/ziho/ganomplayerclient/ClientCommand.java
@@ -0,0 +1,64 @@
+package kr.ziho.ganomplayerclient;
+import java.util.Arrays;
+import java.util.List;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumChatFormatting;
+
+public class ClientCommand implements ICommand {
+
+	GanomPlayerClient mod;
+	String name = "gc";
+	String usage = EnumChatFormatting.RED + "Usage: /" + name;
+	
+	public ClientCommand(GanomPlayerClient mod) {
+		this.mod = mod;
+	}
+
+	@Override
+	public int compareTo(ICommand o) {
+		return 0;
+	}
+
+	@Override
+	public String getCommandName() {
+		return name;
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		return usage;
+	}
+
+	@Override
+	public List<String> getCommandAliases() {
+		return Arrays.asList();
+	}
+
+	@Override
+	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+		if (args[0].equals("connect") || args[0].equals("c")) {
+			mod.connect();
+		} else if (args[0].equals("disconnect") || args[0].equals("dc")) {
+			mod.disconnect();
+		}
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender sender) {
+		return true;
+	}
+
+	@Override
+	public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] args, int index) {
+		return false;
+	}
+
+}
---
+++ src/main/java/kr/ziho/ganomplayerclient/GanomPlayerClient.java
@@ -0,0 +1,117 @@
+package kr.ziho.ganomplayerclient;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.client.ClientCommandHandler;
+
+@Mod(modid = GanomPlayerClient.MODID, version = GanomPlayerClient.VERSION)
+public class GanomPlayerClient {
+	
+    public static final String MODID = "ganomplayerclient";
+    public static final String VERSION = "1.0";
+    
+    public static final String HOST = "127.0.0.1";
+    public static final int PORT = 25567;
+    
+    public static final int framesInTimeline = 10;
+    public static final int frameInterval = 100;  // microseconds
+	
+    private Socket socket;
+    private SocketAddress address;
+    private boolean running = false;
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    	ClientCommandHandler.instance.registerCommand(new ClientCommand(this));
+    }
+    
+    public void connect() {
+    	socket = new Socket();
+        address = new InetSocketAddress(GanomPlayerClient.HOST, GanomPlayerClient.PORT);
+        try {
+        	socket.connect(address);
+        } catch (IOException e) {
+        	String errorMessage = "\u00A7c[GanomPlayerClient] Socket connection failure";
+        	Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(errorMessage));
+        }
+
+        Thread socketThread = new Thread(new SocketThread());
+        socketThread.start();
+        String successMessage = "\u00A7a[GanomPlayerClient] Socket thread is now running";
+    	Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(successMessage));
+    }
+    
+    public void disconnect() {
+    	running = false;
+    }
+    
+    public void behave(JsonObject jsonObj) {
+    	
+    }
+	
+	private class SocketThread implements Runnable {
+		@Override
+		public void run() {
+			running = true;
+			try {
+				InputStream in = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in), 4096);
+                double startTime = System.currentTimeMillis();
+                
+                while (running) {
+    				EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
+    				if (player == null) {
+    					running = false;
+    					continue;
+    				}
+    				// Reconnect if connection was lost
+                    if (!socket.isConnected())
+                        socket.connect(address);
+
+                    // Receive
+                    System.out.println("receiving...");
+                    String line = reader.readLine();
+                    System.out.println("readLine: " + line);
+                    
+                    boolean doBehave = true;
+                    JsonArray frames = new JsonArray();
+                    JsonObject receivedJson = (JsonObject) new JsonParser().parse(line);
+                    frames = (JsonArray) receivedJson.get("frames");
+                    if (frames == null) doBehave = false;
+                    
+                    int count = 1;
+
+                    while (count <= framesInTimeline) {
+                        if (System.currentTimeMillis() >= startTime + frameInterval * count) {
+                            // Make AI behave
+                            if (doBehave)
+                                behave((JsonObject) frames.get(count - 1));
+                            count++;
+                        }
+                    }
+    			}
+                socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+    
+}
